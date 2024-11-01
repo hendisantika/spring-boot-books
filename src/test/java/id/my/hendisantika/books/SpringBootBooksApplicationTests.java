d99ac8074d6bac9b148f39a8a5d88a9061f516b7
hendisantika <hendisantika@yahoo.co.id>
2024-11-01 07:17:53 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/books/SpringBootBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
